83d5994480c228a8ff7631267e1e63ae0e7b0871
Oops19 <Oops19@users.noreply.github.com>
2024-06-06 22:27:12 +0200

    update

--- sit_on_ground/modinfo.py
+++ sit_on_ground/modinfo.py
@@ -34,12 +34,14 @@ class ModInfo(CommonModInfo):
 
     @property
     def _version(self) -> str:
-        return '1.0.2'
+        return '1.0.3'
 
 
-"""
+r'''
 TODO
     Remove testSet_SkirtOrDress_False from .package and use Live XML
+v1.0.3
+    Tested with TS4 v1.107
 v1.0.2
     Update README for new TS4 version
 v1.0.1
@@ -48,4 +50,4 @@ v1.0.0
     Tested version
 v0.0.1
     Initial version
-"""
+'''
